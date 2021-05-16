c17148c1a84a07a9b32a57b06d29c3d3a725f289
sim <cscaesim@gmail.com>
2021-05-16 11:16:11 -0500

    Initial Commit

---
+++ Tap Benchmark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tap Benchmark
+//
+//  Created by Caine Simpson on 5/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
